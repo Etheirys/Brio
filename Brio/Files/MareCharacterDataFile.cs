390b68f2cb3cf2a00a3fef94c4ca9c7438c1fd24
Yuki <yuuki.walsh@outlook.com>
2024-01-26 02:07:01 +1100

    cleanup mcdf file

--- Brio/Files/MareCharacterDataFile.cs
+++ Brio/Files/MareCharacterDataFile.cs
@@ -3,16 +3,18 @@ using Brio.Library.Sources;
 using Brio.Library.Tags;
 using Brio.Resources;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 namespace Brio.Files;
 
+
 [FileType("Mare Character Data", "Images.FileIcon_Mcdf.png", ".mcdf", "Load")]
 internal class MareCharacterDataFile : IFile
 {
+    public string? Description => null;
     public string? Author => null;
-    public string? Description { get; set; }
     public string? Version => null;
     public TagCollection? Tags => null;
 
@@ -23,22 +25,8 @@ internal class MareCharacterDataFile : IFile
 
     public static MareCharacterDataFile? Load(string filePath)
     {
-        /*using var unwrapped = File.OpenRead(filePath);
-        using var lz4Stream = new LZ4Stream(unwrapped, LZ4StreamMode.Decompress, LZ4StreamFlags.HighCompression);
-        using var reader = new BinaryReader(lz4Stream);
-
-        var chars = new string(reader.ReadChars(4));
-        if(!string.Equals(chars, "MCDF", StringComparison.Ordinal))
-            throw new InvalidDataException("Not a Mare Chara File");
-
-        var version = reader.ReadByte();
-        if (version != 1)
-            throw new InvalidDataException("Unsupported Mara Chara file version");
-
-        var dataLength = reader.ReadInt32();
-        byte[] data = reader.ReadBytes(dataLength);
-        string json = Encoding.UTF8.GetString(data);
-        return JsonSerializer.Deserialize<MareCharacterDataFile>(json);*/
-        return null;
+        // No support for actually loading an mcdf, as thats handled by IPC-ing to Mare.
+        // But this class is used for the library tags, so lets just fake it with an empty file. =)
+        return new MareCharacterDataFile();
     }
 }
