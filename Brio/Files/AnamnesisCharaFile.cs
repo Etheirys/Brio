1c4d6da064ac119f2161c74d60ccb31d7ec3115d
Yuki <yuuki.walsh@outlook.com>
2024-02-02 19:45:02 +1100

    Update AnamnesisCharaFile.cs

--- Brio/Files/AnamnesisCharaFile.cs
+++ Brio/Files/AnamnesisCharaFile.cs
@@ -1,15 +1,12 @@
 ﻿using Brio.Capabilities.Actor;
 using Brio.Entities.Actor;
 using Brio.Game.Actor.Appearance;
-using Brio.Library;
-using Brio.Library.Actions;
 using Brio.Library.Sources;
 using Brio.Library.Tags;
 using Brio.Resources;
 using Dalamud.Interface.Internal;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using System;
-using System.Collections.Generic;
 using System.Numerics;
 using System.Threading.Tasks;
 
@@ -20,13 +17,6 @@ internal class AnamnesisCharaFileInfo : JsonDocumentBaseFileInfo<AnamnesisCharaF
     public override string Name => "Character File";
     public override IDalamudTextureWrap Icon => ResourceProvider.Instance.GetResourceImage("Images.FileIcon_Chara.png");
     public override string Extension => ".chara";
-
-    public override void GetLibraryActions(ref List<EntryActionBase> actions)
-    {
-        base.GetLibraryActions(ref actions);
-
-        actions.Add(new ApplyFileToSelectedActorAction<AnamnesisCharaFile>(Apply, true));
-    }
 
     private async Task Apply(FileEntry fileEntry, ActorEntity actor)
     {
