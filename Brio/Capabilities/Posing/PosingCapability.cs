d8d64ac42b12140b678fff2306c45f23c62b6dd3
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2024-03-22 11:10:59 -0600

    Fix model transform being applied when it shouldn't

--- Brio/Capabilities/Posing/PosingCapability.cs
+++ Brio/Capabilities/Posing/PosingCapability.cs
@@ -131,6 +131,9 @@ internal class PosingCapability : ActorCharacterCapability
 
         options ??= _posingService.DefaultImporterOptions;
 
+        if(options.ApplyModelTransform && reset)
+            ModelPosing.ResetTransform();
+
         SkeletonPosing.ImportSkeletonPose(poseFile, options);
         ModelPosing.ImportModelPose(poseFile, options);
 
