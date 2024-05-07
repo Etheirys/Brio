bb39bad1072ccb16111997b2b12f3afd23e8b9da
Kenneth M <KennethMizell@outlook.com>
2024-05-07 13:15:06 -0500

    Fix sig

--- Brio/Game/Posing/SkeletonService.cs
+++ Brio/Game/Posing/SkeletonService.cs
@@ -58,7 +58,7 @@ internal unsafe class SkeletonService : IDisposable
         _updateBonePhysicsHook = hooking.HookFromAddress<UpdateBonePhysicsDelegate>(scanner.ScanText(updateBonePhysicsAddress), UpdateBonePhysicsDetour);
         _updateBonePhysicsHook.Enable();
 
-        var finalizeSkeletonsHook = "48 8B 0D B1 ?? ?? 02 E9 ?? ?? 32 00"; // Framework.TaskRenderGraphicsRender
+        var finalizeSkeletonsHook = "48 8B 0D C1 94 11 02 E9 64 7E 32 00"; // Framework.TaskRenderGraphicsRender
         _finalizeSkeletonsHook = hooking.HookFromAddress<FinalizeSkeletonsDelegate>(scanner.ScanText(finalizeSkeletonsHook), FinalizeSkeletonsHook);
         _finalizeSkeletonsHook.Enable();
 
