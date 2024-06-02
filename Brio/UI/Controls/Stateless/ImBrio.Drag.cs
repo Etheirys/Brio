bd4d18e17e2c6b7665f5817706b598b7f5341f5d
Kenneth M <KennethMizell@outlook.com>
2024-06-01 22:34:43 -0500

    Update ImBrio.Drag.cs

--- Brio/UI/Controls/Stateless/ImBrio.Drag.cs
+++ Brio/UI/Controls/Stateless/ImBrio.Drag.cs
@@ -1,21 +1,13 @@
 ﻿using Brio.Input;
-using Brio.UI.Controls.Core;
 using Dalamud.Interface;
 using ImGuiNET;
-using System.Collections.Generic;
 using System.Numerics;
 
 namespace Brio.UI.Controls.Stateless;
 internal static partial class ImBrio
 {
-    private static readonly HashSet<uint> expanded = [];
-
     public static (bool anyActive, bool didChange) DragFloat3(string label, ref Vector3 vectorValue, float step = 1.0f, FontAwesomeIcon icon = FontAwesomeIcon.None, string tooltip = "")
     {
-        bool changed = false;
-        bool active = false;
-
-
         if(icon == FontAwesomeIcon.None)
         {
             ImGui.Text(label);
@@ -25,75 +17,16 @@ internal static partial class ImBrio
             ImBrio.Icon(icon);
         }
 
-        uint id = ImGui.GetID(label);
-        bool isExpanded = false; //= expanded.Contains(id);
-
-        if(isExpanded)
-        {
-            ImGui.PushStyleColor(ImGuiCol.FrameBg, 0);
-            ImGui.BeginDisabled();
-        }
-
-        Vector2 size = new Vector2(0, 0);
-        size.X = (ImBrio.GetRemainingWidth() - 32) + ImGui.GetStyle().ItemSpacing.X;
-       
         ImGui.SameLine();
-        (var changedf3, var activef3) = DragFloat3Horizontal($"###{id}_drag3", ref vectorValue, step, size);
-        changed |= changedf3;
-        active |= activef3;
 
-        if(isExpanded)
+        uint id = ImGui.GetID(label);
+
+        Vector2 size = new(0, 0)
         {
-            ImGui.EndDisabled();
-            ImGui.PopStyleColor();
-        }
+            X = GetRemainingWidth() - 32 + ImGui.GetStyle().ItemSpacing.X
+        };
 
-        //ImGui.SameLine();
-        //ImGui.SetNextItemWidth(32);
-        //if(ImGui.ArrowButton($"###{label}_decrease", isExpanded ? ImGuiDir.Up : ImGuiDir.Down))
-        //{
-        //    if(isExpanded)
-        //    {
-        //        expanded.Remove(id);
-        //    }
-        //    else
-        //    {
-        //        expanded.Add(id);
-        //    }
-        //}
-
-        if(isExpanded)
-        {
-            float height = (ImBrio.GetLineHeight() * 3) + (ImGui.GetStyle().ItemSpacing.Y * 2) + (ImGui.GetStyle().WindowPadding.Y * 2);
-            if(ImGui.BeginChild($"###{label}_child", new Vector2(0, height), true))
-            {
-                ImGui.PushStyleColor(ImGuiCol.FrameBg, UIConstants.GizmoBlue);
-
-                float x = vectorValue.X;
-                (var pdidChange, var panyActive) = DragFloat($"###{label}_x", ref x, step, $"{tooltip} X");
-                vectorValue.X = x;
-
-                ImGui.PopStyleColor();
-                ImGui.PushStyleColor(ImGuiCol.FrameBg, UIConstants.GizmoGreen);
-
-                float y = vectorValue.Y;
-                (var rdidChange, var ranyActive) = DragFloat($"###{label}_y", ref y, step, $"{tooltip} Y");
-                vectorValue.Y = y;
-
-                ImGui.PopStyleColor();
-                ImGui.PushStyleColor(ImGuiCol.FrameBg, UIConstants.GizmoRed);
-
-                float z = vectorValue.Z;
-                (var sdidChange, var sanyActive) = DragFloat($"###{label}_z", ref z, step, $"{tooltip} Z");
-                vectorValue.Z = z;
-
-                changed |= pdidChange |= rdidChange |= sdidChange;
-                active |= panyActive |= ranyActive |= sanyActive;
-
-                ImGui.PopStyleColor();
-                ImGui.EndChild();
-            }
-        }
+        (bool changed, bool active) = DragFloat3Horizontal($"###{id}_drag3", ref vectorValue, step, size);
 
         return (active, changed);
     }
@@ -117,54 +50,48 @@ internal static partial class ImBrio
 
         changed |= ImGui.DragFloat($"##{label}_X", ref value.X, step / 10);
         if(ImGui.IsItemHovered())
-            ImGui.SetTooltip("X");
-        active |= ImGui.IsItemActive();
-
-        if (ImGui.IsItemHovered())
         {
+            ImGui.SetTooltip("X");
             float mouseWheel = ImGui.GetIO().MouseWheel / 10;
-            if (mouseWheel != 0) 
+            if(mouseWheel != 0)
             {
                 value.X += mouseWheel * step;
                 changed = true;
             }
         }
+        active |= ImGui.IsItemActive();
 
         ImGui.SameLine();
         ImGui.SetNextItemWidth(entryWidth);
 
         changed |= ImGui.DragFloat($"##{label}_Y", ref value.Y, step / 10);
         if(ImGui.IsItemHovered())
-            ImGui.SetTooltip("Y");
-        active |= ImGui.IsItemActive();
-
-        if (ImGui.IsItemHovered())
         {
+            ImGui.SetTooltip("Y");
             float mouseWheel = ImGui.GetIO().MouseWheel / 10;
-            if (mouseWheel != 0) 
+            if(mouseWheel != 0)
             {
                 value.Y += mouseWheel * step;
                 changed = true;
             }
         }
+        active |= ImGui.IsItemActive();
 
         ImGui.SameLine();
         ImGui.SetNextItemWidth(entryWidth);
 
         changed |= ImGui.DragFloat($"##{label}_Z", ref value.Z, step / 10);
         if(ImGui.IsItemHovered())
-            ImGui.SetTooltip("Z");
-        active |= ImGui.IsItemActive();
-
-        if (ImGui.IsItemHovered())
         {
+            ImGui.SetTooltip("Z");
             float mouseWheel = ImGui.GetIO().MouseWheel / 10;
-            if (mouseWheel != 0) 
+            if(mouseWheel != 0)
             {
                 value.Z += mouseWheel * step;
                 changed = true;
             }
         }
+        active |= ImGui.IsItemActive();
 
         return (active, changed);
     }
@@ -210,10 +137,10 @@ internal static partial class ImBrio
             ImGui.SetTooltip($"{tooltip}");
         active |= ImGui.IsItemActive();
 
-        if (ImGui.IsItemHovered())
+        if(ImGui.IsItemHovered())
         {
             float mouseWheel = ImGui.GetIO().MouseWheel / 10;
-            if (mouseWheel != 0) 
+            if(mouseWheel != 0)
             {
                 value += mouseWheel * step;
                 changed = true;
