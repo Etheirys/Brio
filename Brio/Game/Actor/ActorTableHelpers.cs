f7f124c3d8c60336a591591cd6d847b173597821
Asgard <git@asgard.io>
2024-11-20 02:31:27 -0700

    Fix actor count

--- Brio/Game/Actor/ActorTableHelpers.cs
+++ Brio/Game/Actor/ActorTableHelpers.cs
@@ -5,5 +5,5 @@ internal class ActorTableHelpers
     public const int OverworldStart = 0;
     public const int OverworldEnd = 199;
     public const int GPoseStart = 201; // This is really 200, but we're skipping the special one for now
-    public const int GPoseEnd = 240;
+    public const int GPoseEnd = 439;
 }
