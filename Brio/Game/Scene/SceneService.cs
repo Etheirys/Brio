84c3944f73a258f4c5f13dd1e786b6447b208328
Bronya-Rand <azarieldc@gmail.com>
2025-02-20 01:48:32 +0000

    chore: cleanup

--- Brio/Game/Scene/SceneService.cs
+++ Brio/Game/Scene/SceneService.cs
@@ -158,20 +158,6 @@ public class SceneService(EntityManager _entityManager, VirtualCameraManager _vi
         }, delayTicks: 2);
     }
 
-    //private static void ImportShadersFromFile(ref ModelShaderOverride modelShaderOverride, BrioHuman.ShaderParams shaderParams)
-    //{
-    //    modelShaderOverride.SkinColor = shaderParams.SkinColor;
-    //    modelShaderOverride.SkinGloss = shaderParams.SkinGloss;
-    //    modelShaderOverride.MuscleTone = shaderParams.MuscleTone;
-    //    modelShaderOverride.MouthColor = shaderParams.MouthColor;
-    //    modelShaderOverride.HairColor = shaderParams.HairColor;
-    //    modelShaderOverride.HairGloss = shaderParams.HairGloss;
-    //    modelShaderOverride.HairHighlight = shaderParams.HairHighlight;
-    //    modelShaderOverride.LeftEyeColor = shaderParams.LeftEyeColor;
-    //    modelShaderOverride.RightEyeColor = shaderParams.RightEyeColor;
-    //    modelShaderOverride.FeatureColor = shaderParams.FeatureColor;
-    //}
-
     private async Task ApplyDataToActor(EntityId actorId, ActorFile actorFile)
     {
         var attachedActor = _entityManager.GetEntity<ActorEntity>(actorId)!;
