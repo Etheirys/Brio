57be0ac1213a7f62987b49c468baf233869ad4f0
Bronya-Rand <azarieldc@gmail.com>
2025-03-17 01:02:18 +0000

    feat: add dragfloat2 for vector3's and degree mode

--- Brio/Core/BrioUtilities.cs
+++ Brio/Core/BrioUtilities.cs
@@ -23,11 +23,17 @@ public static class BrioUtilities
 
     public static float DegreesToRadians(float degrees)
     {
+        if(degrees == 0)
+            return 0;
+
         return degrees * (float)(System.Math.PI / 180);
     }
 
     public static float RadiansToDegrees(float radians)
     {
+        if(radians == 0)
+            return 0;
+
         return radians * (float)(180 / System.Math.PI);
     }
 }
