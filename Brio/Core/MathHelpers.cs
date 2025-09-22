50489f4552eec5225fcd704428718df59429fd40
Minmoose <KennethBohr@outlook.com>
2025-09-22 15:09:52 -0500

    Update MathHelpers.cs

--- Brio/Core/MathHelpers.cs
+++ Brio/Core/MathHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Brio.Core;
 
@@ -6,4 +7,7 @@ public static class MathHelpers
 {
     public const float DegreesToRadians = MathF.PI / 180.0f;
     public const float RadiansToDegrees = 180.0f / MathF.PI;
+   
+    public const float Deg2Rad = MathF.PI * 2.0f / 360.0f;
+    public const float Rad2Deg = 1.0f / Deg2Rad;
 }
