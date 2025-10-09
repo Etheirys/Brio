35b63bde85fe26b4c10cd6e4cd8a5006be5d0ff1
Bronya-Rand <azarieldc@gmail.com>
2025-10-09 08:21:41 +0100

    chore: prototype of translation code (wip)

--- Brio/Game/Camera/BrioCamera.cs
+++ Brio/Game/Camera/BrioCamera.cs
@@ -36,7 +36,74 @@ public struct BrioCamera
 
     [FieldOffset(0x218)] public Vector2 Collide;
 
-    public readonly Vector3 RotationAsVector3 => new(Angle.X - Pan.X, -Angle.Y - Pan.Y, Roll);
+    // Converts Brio Camera Angle and Pan Vector2s to Vector3 for Free Camera Rotation
+    // using Cubic Polynomials and Quadratic Equations
+    private readonly Vector3 ConvertBrioCameraToXIVCamera()
+    { 
+        const float cubicACoefficient = 0.3083f;
+        const float cubicBCoefficient = -3.5417f;
+        const float cubicCCoefiicient = 14.5646f;
+        const float cubicDCoefficient = -24.6688f;
+
+        const float quadraticACoefficient = -67.7f;
+        const float quadraticBCoefficient = 72.45f;
+
+        const float CONSTANT_X_OFFSET = 57.3f;
+
+        // Calculate offset based on Zoom level
+        // Yes.. this affects every calculation below
+        // This will be used as the C coefficient
+        float panYOffset = (float)((cubicACoefficient * Math.Pow(2.5, 3)) +
+                           (cubicBCoefficient * Math.Pow(2.5, 2)) +
+                           (cubicCCoefiicient * 2.5) +
+                           cubicDCoefficient);
+
+        Brio.Log.Verbose($"Zoom: {Zoom}, PanYOffset: {panYOffset}");
+
+        Vector3 rotation;
+        rotation.X = 0f;
+
+        float newXDegrees;
+        if (Angle.X == Pan.X)
+        {
+            newXDegrees = 0f;
+        } else
+        {
+            newXDegrees = CONSTANT_X_OFFSET * (Angle.X - Pan.X);
+        }
+
+        float newYDegrees;
+        if((Angle.Y + Pan.Y < -2f) || (Angle.Y + Pan.Y > 2f))
+        {
+            // cap the angle and pan as beyond is position changes
+            // cap A at 1 (multiplication), B at 2 (addition)
+            if(Angle.Y + Pan.Y > 2f)
+            {
+                newYDegrees = (quadraticACoefficient * 1f) + (quadraticBCoefficient * 2f) + panYOffset; 
+            }
+            else
+            {
+                newYDegrees = (quadraticACoefficient * -1f) + (quadraticBCoefficient * -2f) + panYOffset;
+            }
+        }
+        else
+        {
+            newYDegrees = (quadraticACoefficient * (Angle.Y * Pan.Y)) + (quadraticBCoefficient * (Angle.Y + Pan.Y)) + panYOffset;
+        }
+        newYDegrees = -newYDegrees; // Invert for XIV Camera
+
+        // convert to radians
+        float newXRadians = newXDegrees * MathHelpers.DegreesToRadians;
+        float newYRadians = newYDegrees * MathHelpers.DegreesToRadians;
+
+        rotation.X = newXRadians;
+        rotation.Y = newYRadians;
+        rotation.Z = Roll;
+
+        return rotation;
+    }
+
+    public readonly Vector3 RotationAsVector3 => ConvertBrioCameraToXIVCamera(); //new(Angle.X - Pan.X, -Angle.Y - Pan.Y, Roll);
 
     public readonly Quaternion CalculateDirectionAsQuaternion()
         => (new Vector3(-(Angle.Y + Pan.Y), ((Angle.X + MathF.PI) % MathF.Tau) - Pan.X, 0.0f)
