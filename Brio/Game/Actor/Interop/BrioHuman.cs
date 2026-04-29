b1dd9c67e885b1cff8d318867f546cd2d5f90204
Minmoose <12839703+Minmoose@users.noreply.github.com>
2026-04-29 17:33:25 -0500

    my head hurts, someone get me some Nyquil

--- Brio/Game/Actor/Interop/BrioHuman.cs
+++ Brio/Game/Actor/Interop/BrioHuman.cs
@@ -29,28 +29,22 @@ public struct BrioHuman
         [FieldOffset(0x0C)]
         public float MuscleTone;
 
-        //[FieldOffset(0x10)]
-        //public Vector3 SkinGloss;
-
         [FieldOffset(0x10)]
         public Vector4 MouthColor;
 
-        [FieldOffset(0x30)]
+        [FieldOffset(0x20)]
         public Vector3 HairColor;
 
-        //[FieldOffset(0x40)]
-        //public Vector3 HairGloss;
-
-        [FieldOffset(0x40)]
+        [FieldOffset(0x30)]
         public Vector3 HairHighlight;
+      
+        [FieldOffset(0x40)]
+        public Vector3 RightEyeColor;
 
         [FieldOffset(0x50)]
         public Vector3 LeftEyeColor;
 
         [FieldOffset(0x60)]
-        public Vector3 RightEyeColor;
-
-        [FieldOffset(0x70)]
         public Vector3 FeatureColor;
     }
 }
