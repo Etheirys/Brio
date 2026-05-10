c3f708485dd87901e72d0ee2f9c702a45b39945f
Minmoose <12839703+Minmoose@users.noreply.github.com>
2026-05-10 18:34:09 -0500

    Update EnvLifetimeWidget

--- Brio/Capabilities/World/Environment/EnvironmentLifetimeCapability.cs
+++ Brio/Capabilities/World/Environment/EnvironmentLifetimeCapability.cs
@@ -1,34 +1,15 @@
 ﻿using Brio.Entities.Core;
-using Brio.Game.Actor;
-using Brio.Game.Camera;
-using Brio.Game.World;
+using Brio.Game.GPose;
 using Brio.UI.Widgets.World;
-using Brio.UI.Windows.Specialized;
 
 namespace Brio.Capabilities.World;
 
 public class EnvironmentLifetimeCapability : LightCapability
 {
-    private readonly LightingService _lightingService;
-    private readonly LightWindow _lightWindow;
-
-    public EnvironmentLifetimeCapability(Entity parent, ActorSpawnService actorSpawnService, VirtualCameraManager cameraManager, LightingService lightingService, LightWindow lightWindow) : base(parent)
+    public readonly GPoseService GPoseService;
+    public EnvironmentLifetimeCapability(Entity parent, GPoseService gPoseService) : base(parent)
     {
-        _lightingService = lightingService;
-        _lightWindow = lightWindow;
-
-        this.Widget = new EnvLifetimeWidget(this, actorSpawnService, cameraManager, lightingService);
+        GPoseService = gPoseService;
+        this.Widget = new EnvLifetimeWidget(this);
     }
-
-    public bool IsLightWindowOpen => _lightWindow.IsOpen;
-
-    public void ToggleLightWindow()
-    {
-        _lightWindow.IsOpen = !_lightWindow.IsOpen;
-    }
-    public void OpenLightWindow()
-    {
-        _lightWindow.IsOpen = true;
-    }
-
 }
