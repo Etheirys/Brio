153edd211a8d39809830f6b4332d0adb864a84c4
Minmoose <12839703+Minmoose@users.noreply.github.com>
2026-07-06 10:09:00 -0500

    Slightly update the spawn menu with a better look and add `Add light` & `Add Actor` from world

--- Brio/UI/Controls/Editors/SpawnMenu.cs
+++ Brio/UI/Controls/Editors/SpawnMenu.cs
@@ -3,6 +3,7 @@ using Brio.Entities.Camera;
 using Brio.Entities.Core;
 using Brio.Game.Actor;
 using Brio.Game.Camera;
+using Brio.Game.Core;
 using Brio.Game.World;
 using Brio.Game.World.Interop;
 using Brio.Game.WorldObjects;
@@ -15,6 +16,8 @@ using Dalamud.Interface.Utility;
 using Dalamud.Interface.Utility.Raii;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace Brio.UI.Controls.Editors;
@@ -31,6 +34,7 @@ public class SpawnMenu
     private static WorldObjectService _worldObjectService => Instance!.provider.GetRequiredService<WorldObjectService>();
     private static EntityManager _entityManager => Instance!.provider.GetRequiredService<EntityManager>();
     private static ReferenceImageService _referenceImageService => Instance!.provider.GetRequiredService<ReferenceImageService>();
+    private static ObjectMonitorService _objectMonitorService => Instance!.provider.GetRequiredService<ObjectMonitorService>();
 
     static SpawnMenu Instance = null!;
     public void Initialize(IServiceProvider serviceProvider)
@@ -69,6 +73,8 @@ public class SpawnMenu
         if(popup.Success is false)
             return;
 
+        ImBrio.BlurPopup();
+
         var buttonSize = new Vector2(MenuWidth * ImGuiHelpers.GlobalScale, 0);
 
         using(ImRaii.PushColor(ImGuiCol.Button, UIConstants.Transparent))
@@ -77,14 +83,45 @@ public class SpawnMenu
             {
                 ImBrio.SeparatorText("Actors");
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.User, "Actor", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.User, "Actor", buttonSize))
                 {
                     _actorSpawnService.CreateCharacter(out _, SpawnFlags.Default, true);
                 }
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.PlusSquare, "Actor with Companion", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.PlusSquare, "Actor with Companion", buttonSize))
                 {
                     _actorSpawnService.CreateCharacter(out _, SpawnFlags.WithCompanionSlot, false);
+                }
+
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Globe, "Actor from World...", buttonSize))
+                {
+                    ImGui.OpenPopup("FromWorldPopup");
+                }
+
+                using var fromWorldPopup = ImRaii.Popup("FromWorldPopup");
+                if(fromWorldPopup.Success)
+                {
+                    var playerPosition = _objectMonitorService.ObjectTable.LocalPlayer?.Position ?? Vector3.Zero; // I hate this
+                    var overworldActors = _objectMonitorService.GetOverworldActors().OrderBy(actor => Vector3.DistanceSquared(playerPosition, actor.Position));
+
+                    if(!overworldActors.Any())
+                    {
+                        ImGui.TextDisabled("No world actors found");
+                    }
+
+                    foreach(var actor in overworldActors)
+                    {
+                        if(actor == null || !actor.IsValid())
+                            return;
+
+                        var distanceText = $" [{Vector3.Distance(playerPosition, actor.Position):0.0}]";
+
+                        if(ImGui.MenuItem(string.IsNullOrWhiteSpace(actor?.Name.ToString()) ? $"Unknown {distanceText}##actor_{actor!.GameObjectId}" : $"{actor.Name} {distanceText}##actor_{actor.GameObjectId}"))
+                        {
+                            _actorSpawnService.AddFromWorld(actor!);
+                            ImGui.CloseCurrentPopup();
+                        }
+                    }
                 }
             }
 
@@ -93,7 +130,13 @@ public class SpawnMenu
                 ImGui.Spacing();
                 ImBrio.SeparatorText("Objects");
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Cubes, "Prop", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Couch, "Open Object Catalog", buttonSize))
+                {
+                    UIManager.Instance.ToggleFurnitureCatalogWindow();
+                    ImGui.CloseCurrentPopup();
+                }
+
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Cubes, "Prop", buttonSize))
                 {
                     _worldObjectService.SpawnProp(new FFXIVClientStructs.FFXIV.Client.Graphics.Scene.WeaponCreateInfo
                     {
@@ -110,28 +153,19 @@ public class SpawnMenu
                     });
                 }
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Couch, "Open Object Catalog", buttonSize))
-                {
-                    UIManager.Instance.ToggleFurnitureCatalogWindow();
-                    ImGui.CloseCurrentPopup();
-                }
-
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Chair, "Furniture Item", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Chair, "Furniture Item", buttonSize))
                 {
                     _worldObjectService.SpawnFurniture("bgcommon/hou/outdoor/general/0332/asset/gar_b0_m0332.sgb");
-                    ImGui.CloseCurrentPopup();
                 }
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Boxes, "World Object", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Boxes, "World Object", buttonSize))
                 {
                     _worldObjectService.SpawnBgObject("bg/ffxiv/fst_f1/twn/common/bgparts/f1t0_a0_taru1.mdl");
-                    ImGui.CloseCurrentPopup();
                 }
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Burst, "VFX", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Burst, "VFX", buttonSize))
                 {
                     _worldObjectService.SpawnStaticVfx("bgcommon/world/common/vfx_for_bg/eff/val_obj001_o.avfx");
-                    ImGui.CloseCurrentPopup();
                 }
             }
 
@@ -140,19 +174,73 @@ public class SpawnMenu
                 ImGui.Spacing();
                 ImBrio.SeparatorText("Lights");
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Lightbulb, "Spot Light", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Lightbulb, "Spot Light", buttonSize))
                 {
                     _lightingService.SpawnLight(LightType.SpotLight);
                 }
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Lightbulb, "Point Light", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Lightbulb, "Point Light", buttonSize))
                 {
                     _lightingService.SpawnLight(LightType.PointLight);
                 }
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Lightbulb, "Flat Light", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Lightbulb, "Flat Light", buttonSize))
                 {
                     _lightingService.SpawnLight(LightType.FlatLight);
+                }
+
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Globe, "Light from World...", buttonSize))
+                {
+                    ImGui.OpenPopup("FromWorldLightPopup");
+                }
+
+                using var fromWorldLightPopup = ImRaii.Popup("FromWorldLightPopup");
+                if(fromWorldLightPopup.Success)
+                {
+                    var playerPosition = _objectMonitorService.ObjectTable.LocalPlayer?.Position ?? Vector3.Zero; // I hate this
+                    var worldLights = new List<(nint, float)>();
+
+                    unsafe
+                    {
+                        foreach(var light in _lightingService.WorldLights)
+                        {
+                            var blight = (BrioLight*)light;
+                            worldLights.Add((light, Vector3.Distance(playerPosition, blight->Transform.Position)));
+                        }
+
+                        if(worldLights.Count == 0)
+                        {
+                            ImGui.TextDisabled("No world lights found");
+                        }
+                        else
+                        {
+                            if(ImGui.MenuItem($"Add All ({worldLights.Count})###containerwidgetpopup_addAllWorldLights"))
+                            {
+                                if(worldLights.Count == 0)
+                                    return;
+
+                                var folder = _entityManager.CreateEntityOnEntityContainer<FolderEntity>("World Lights");
+                                folder.IsEditable = false;
+
+                                foreach(var (light, _) in worldLights)
+                                {
+                                    var entity = _lightingService.AddWorldLight((BrioLight*)light);
+                                    if(entity != null)
+                                        _entityManager.AttachEntity(entity, folder, autoDetach: true);
+                                }
+
+                                ImGui.CloseCurrentPopup();
+                            }
+                            ImGui.Separator();
+                            foreach(var (light, distance) in worldLights)
+                            {
+                                if(ImGui.MenuItem($"Light: {distance:F1}y##worldlight_{light}"))
+                                {
+                                    _lightingService.AddWorldLight((BrioLight*)light);
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
@@ -161,13 +249,13 @@ public class SpawnMenu
                 ImGui.Spacing();
                 ImBrio.SeparatorText("Cameras");
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Camera, "Brio Camera", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Camera, "Brio Camera", buttonSize))
                 {
                     _cameraManager.CreateCamera(CameraType.Game);
                     ImGui.CloseCurrentPopup();
                 }
 
-                if(ImBrio.DrawIconButton(FontAwesomeIcon.Video, "Free-Cam", buttonSize))
+                if(ImBrio.IconButtonWithText(FontAwesomeIcon.Video, "Free-Cam", buttonSize))
                 {
                     _cameraManager.CreateCamera(CameraType.Free);
                     ImGui.CloseCurrentPopup();
@@ -177,12 +265,13 @@ public class SpawnMenu
             ImGui.Spacing();
             ImBrio.SeparatorText("Other");
 
-            if(ImBrio.DrawIconButton(FontAwesomeIcon.Image, "Reference Image", buttonSize))
+            if(ImBrio.IconButtonWithText(FontAwesomeIcon.Image, "Reference Image", buttonSize))
             {
                 ImGui.CloseCurrentPopup();
+
                 UIManager.Instance.FileDialogManager.OpenFileDialog(
                     "Select Reference Image###ref_img_picker",
-                    "Images{.png,.jpg,.jpeg,.bmp}",
+                    "Images{.png,.jpg,.jpeg}",
                     (success, path) =>
                     {
                         if(success)
@@ -190,13 +279,11 @@ public class SpawnMenu
                     });
             }
 
-            if(ImBrio.DrawIconButton(FontAwesomeIcon.FolderPlus, "Folder", buttonSize))
+            if(ImBrio.IconButtonWithText(FontAwesomeIcon.FolderPlus, "Folder", buttonSize))
             {
-                _entityManager.CreateEntityOnEntityContainer<FolderEntity>($"Folder ({foldersCount++})");
+                _entityManager.CreateEntityOnEntityContainer<FolderEntity>($"Folder");
             }
         }
     }
-
-    static int foldersCount = 0;
 }
 
