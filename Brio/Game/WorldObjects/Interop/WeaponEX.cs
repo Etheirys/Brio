f556b690ae979b3923f93a1fbe32520b1c6c5639
Minmoose <12839703+Minmoose@users.noreply.github.com>
2026-07-06 10:42:31 -0500

    This isn't used. add `WeaponEX.cs`

---
+++ Brio/Game/WorldObjects/Interop/WeaponEX.cs
@@ -0,0 +1,14 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Brio.Game.WorldObjects.Interop;
+
+[StructLayout(LayoutKind.Explicit, Size = 2656)]
+public unsafe struct WeaponEX
+{
+    [FieldOffset(0x00)] public Weapon* BaseObject;
+
+}
