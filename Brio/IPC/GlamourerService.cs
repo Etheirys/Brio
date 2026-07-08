0b788d3f5ced71ec9d149e466f0d1dc37c7e52a6
Minmoose <12839703+Minmoose@users.noreply.github.com>
2026-07-08 11:46:22 -0500

    Fix it, Now with sleep

--- Brio/IPC/GlamourerService.cs
+++ Brio/IPC/GlamourerService.cs
@@ -117,7 +117,7 @@ public class GlamourerService : BrioIPC
 
     public bool CheckForLock(IGameObject? character)
     {
-        if(IsAvailable == false || character is null || _gPoseService.IsGPosing == false)
+        if(IsAvailable == false || character is null)
             return false;
 
         var (key, _) = _glamourerGetState.Invoke(character!.ObjectIndex);
